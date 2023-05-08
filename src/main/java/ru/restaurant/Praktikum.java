efff95625c12f3348d422d68b9c21c54bf8b8f17
x-oe <ozz91@yandex.ru>
2023-05-08 15:16:51 +0300

    first commit

--- src/main/java/ru/restaurant/Praktikum.java
+++ src/main/java/ru/restaurant/Praktikum.java
@@ -1,2 +1,13 @@
-package ru.restaurant;public class Praktikum {
+package ru.restaurant;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Restaurant restaurant = new Restaurant(); // создай объект класса Restaurant
+        restaurant.showMenu();
+        restaurant.cook("Салат из груш и лисичек");
+        restaurant.checkPears();
+
+        // вызови три написанных ранее метода
+    }
 }
--- src/main/java/ru/restaurant/Restaurant.java
+++ src/main/java/ru/restaurant/Restaurant.java
@@ -1,2 +1,28 @@
-package ru.restaurant;public class Restaurant {
-}
+package ru.restaurant;
+
+public class Restaurant {
+
+    int pearsCount;
+    String[] menu = {"Шарлотка грушевая", "Компот грушевый", "Салат из груш и лисичек", "Рулет с грушами"};
+
+    public void showMenu() {
+        for (int i = 0; i < menu.length; i = i + 1) {
+            System.out.println("Сегодня у нас в меню " + menu[i]);
+        }
+    }
+
+    public void checkPears() {
+        if (pearsCount < 5) {
+            System.out.println("Пора заказать ещё груш");
+            pearsCount = pearsCount + 5;
+        } else {
+            System.out.println("Груш пока достаточно");
+        }
+    }
+
+    public void cook(String name) {
+        System.out.println("Готовим " + name);
+        pearsCount = pearsCount - 1;
+    }
+
+    }
